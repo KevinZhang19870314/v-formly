3db85e85df343f2b35d966e62d99591a8d4406fe
kevin.zhang <kevin.zhang@cardinfolink.com>
2022-10-11 11:31:07 +0800

    chore: fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,164 +1,178 @@
-const path = require('path')
+const path = require("path");
 
-module.exports = ctx => ({
-    dest: 'vuepress',
+module.exports = (ctx) => ({
+  dest: "vuepress",
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "v-formly",
+      description: "v-formly is a dynamic (JSON powered) form library for vue.",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "v-formly",
+      description: "v-formly是vue的动态（JSON驱动）表单库。",
+    },
+  },
+  head: [
+    ["link", { rel: "icon", href: `/logo.png` }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: `/icons/apple-touch-icon-152x152.png` },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }],
+  ],
+  // theme: '@vuepress/vue',
+  themeConfig: {
+    repo: "KevinZhang19870314/v-formly",
+    editLinks: true,
+    docsDir: "docs",
+    // #697 Provided by the official algolia team.
+    // algolia: ctx.isProd ? ({
+    //     apiKey: '3a539aab83105f01761a137c61004d85',
+    //     indexName: 'vuepress',
+    //     algoliaOptions: {
+    //         facetFilters: ['tags:v1']
+    //     }
+    // }) : null,
+    smoothScroll: true,
     locales: {
-        '/': {
-            lang: 'en-US',
-            title: 'v-formly',
-            description: 'v-formly is a dynamic (JSON powered) form library for vue.'
+      "/": {
+        label: "English",
+        selectText: "Languages",
+        ariaLabel: "Select language",
+        editLinkText: "Edit this page on GitHub",
+        lastUpdated: "Last Updated",
+        nav: [
+          {
+            text: "Guide",
+            link: "/guide/",
+          },
+          {
+            text: "Config Reference",
+            link: "/components/",
+          },
+        ],
+        sidebar: {
+          "/guide/": getGuideSidebar("Guide", "Advanced"),
+          "/components/": getComponentsSidebar("Components", "Advanced"),
         },
-        '/zh/': {
-            lang: 'zh-CN',
-            title: 'v-formly',
-            description: 'v-formly是vue的动态（JSON驱动）表单库。'
-        }
-    },
-    head: [
-        ['link', { rel: 'icon', href: `/logo.png` }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-    ],
-    // theme: '@vuepress/vue',
-    themeConfig: {
-        repo: 'KevinZhang19870314/v-formly',
-        editLinks: true,
-        docsDir: 'docs',
-        // #697 Provided by the official algolia team.
-        // algolia: ctx.isProd ? ({
-        //     apiKey: '3a539aab83105f01761a137c61004d85',
-        //     indexName: 'vuepress',
-        //     algoliaOptions: {
-        //         facetFilters: ['tags:v1']
-        //     }
-        // }) : null,
-        smoothScroll: true,
-        locales: {
-            '/': {
-                label: 'English',
-                selectText: 'Languages',
-                ariaLabel: 'Select language',
-                editLinkText: 'Edit this page on GitHub',
-                lastUpdated: 'Last Updated',
-                nav: [
-                    {
-                        text: 'Guide',
-                        link: '/guide/'
-                    },
-                    {
-                        text: 'Config Reference',
-                        link: '/components/'
-                    }
-                ],
-                sidebar: {
-                    '/guide/': getGuideSidebar('Guide', 'Advanced'),
-                    '/components/': getComponentsSidebar('Components', 'Advanced'),
-                }
-            },
-            '/zh/': {
-                label: '简体中文',
-                selectText: '选择语言',
-                ariaLabel: '选择语言',
-                editLinkText: '在 GitHub 上编辑此页',
-                lastUpdated: '上次更新',
-                nav: [
-                    {
-                        text: '指南',
-                        link: '/zh/guide/'
-                    },
-                    {
-                        text: '组件',
-                        link: '/zh/components/'
-                    }
-                ],
-                sidebar: {
-                    '/zh/guide/': getGuideSidebar('指南', '深入'),
-                    '/zh/components/': getComponentsSidebar('组件', '高级'),
-                }
-            }
-        }
-    },
-    plugins: [
-        ['@vuepress/back-to-top', true],
-        ['@vuepress/pwa', {
-            serviceWorker: true,
-            updatePopup: true
-        }],
-        ['@vuepress/medium-zoom', true],
-        ['container', {
-            type: 'vue',
-            before: '<pre class="vue-container"><code>',
-            after: '</code></pre>'
-        }],
-        ['container', {
-            type: 'upgrade',
-            before: info => `<UpgradePath title="${info}">`,
-            after: '</UpgradePath>'
-        }],
-        ['demo-container']
-    ],
-    chainWebpack: config => {
-        config.resolve.alias.set('core-js/library/fn', 'core-js/features')
-    },
-    configureWebpack: {
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '../../src/'),
-            }
-        }
-    }
-})
-
-function getComponentsSidebar(groupA, groupB) {
-    return [
-        {
-            title: groupA,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: [
-                '',
-                'string',
-                'boolean'
-            ]
+      },
+      "/zh/": {
+        label: "简体中文",
+        selectText: "选择语言",
+        ariaLabel: "选择语言",
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdated: "上次更新",
+        nav: [
+          {
+            text: "指南",
+            link: "/zh/guide/",
+          },
+          {
+            text: "组件",
+            link: "/zh/components/",
+          },
+        ],
+        sidebar: {
+          "/zh/guide/": getGuideSidebar("指南", "深入"),
+          "/zh/components/": getComponentsSidebar("组件", "高级"),
         },
-        {
-            title: groupB,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: [
-                'advanced'
-            ]
-        }
-    ]
-}
+      },
+    },
+  },
+  plugins: [
+    ["@vuepress/back-to-top", true],
+    [
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: true,
+      },
+    ],
+    ["@vuepress/medium-zoom", true],
+    [
+      "container",
+      {
+        type: "vue",
+        before: '<pre class="vue-container"><code>',
+        after: "</code></pre>",
+      },
+    ],
+    [
+      "container",
+      {
+        type: "upgrade",
+        before: (info) => `<UpgradePath title="${info}">`,
+        after: "</UpgradePath>",
+      },
+    ],
+    ["demo-container"],
+  ],
+  chainWebpack: (config) => {
+    config.resolve.alias.set("core-js/library/fn", "core-js/features");
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "../../src/"),
+      },
+    },
+  },
+});
 
 function getGuideSidebar(groupA, groupB) {
-    return [
-        {
-            title: groupA,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: [
-                '',
-                'meta',
-                'terms'
-            ]
-        },
-        {
-            title: groupB,
-            collapsable: false,
-            sidebarDepth: 2,
-            children: [
-                'custom-validator',
-                'layout',
-                'form-submit'
-            ]
-        }
-    ]
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: ["", "meta", "terms"],
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: ["custom-validator", "layout", "form"],
+    },
+  ];
+}
+
+function getComponentsSidebar(groupA, groupB) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: ["", "string", "boolean"],
+    },
+    {
+      title: groupB,
+      collapsable: false,
+      sidebarDepth: 2,
+      children: ["custom-components"],
+    },
+  ];
 }
--- docs/zh/components/README.md
+++ docs/zh/components/README.md
@@ -1,13 +1,11 @@
 # 组件介绍
 
-v-formly 内置了一些基础组件，包括常用的输入框、选择框、单选、多选、下拉框、上传等组件，覆盖了大多数业务，如果这些内置组件不足以满足你的业务，你也可以[自定义组件](TODO)。
+v-formly 内置了一些基础组件，包括常用的输入框、选择框、单选、多选、下拉框、上传等组件，覆盖了大多数业务所需要的组件需求，如果这些内置组件不足以满足你的业务，你也可以[自定义组件](TODO)。
 
-::: tip
-v-formly 中的组件用法尽可能的与AntDv中的保持一致，少数不一致的地方请参考具体组件API。
-
-内置组件与AntDv组件一一对应关系如下：
+v-formly 中的组件用法尽可能的与 AntDv 中的保持一致，少数不一致的地方请参考具体组件 API。内置组件与 AntDv 组件一一对应关系如下：
 
 [String 文本框](/zh/components/string.html) -> [Input 输入框](https://1x.antdv.com/components/input-cn/)
 
+[Boolean 开关](/zh/components/boolean.html) -> [Switch 开关](https://1x.antdv.com/components/switch-cn/)
+
 // TODO 列出剩余的组件关系
-:::
