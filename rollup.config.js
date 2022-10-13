d35ba833687c2e8d0e085f09913c131be9bc91bf
kevin.zhang <kevin.zhang@cardinfolink.com>
2022-10-13 16:15:59 +0800

    feat: rollup pkg

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import path from "path";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import vue from "rollup-plugin-vue";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import alias from "@rollup/plugin-alias";
+import postcss from "rollup-plugin-postcss";
+
+const config = {
+  input: "./src/formly.js",
+  output: {
+    file: "./dist/v-formly-umd.js",
+    exports: "named",
+    format: "umd",
+    name: "v-formly",
+    globals: {
+      vue: "Vue",
+    },
+  },
+  external: ["ajv", "vue", "core-js", "ant-design-vue", /@babel\/runtime/],
+  plugins: [
+    nodeResolve(),
+    postcss(),
+    vue(),
+    alias({
+      entries: {
+        ["@"]: path.resolve(__dirname, "src"),
+      },
+    }),
+    babel({
+      exclude: "**/node_modules/**",
+      babelHelpers: "runtime",
+    }),
+    commonjs(),
+  ],
+};
+
+export default config;
