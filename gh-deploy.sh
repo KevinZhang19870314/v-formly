12eee5c2ec4527b5da8cfab5511fd636880bd9a3
kevin.zhang <kevin.zhang@cardinfolink.com>
2022-10-14 13:50:57 +0800

    chore: fix

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:kevinzhang19870314/v-formly.git master:gh-pages
